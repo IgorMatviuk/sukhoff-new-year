84944a66bdc3df93943f623826d98fe6ae8aed1f
Ihor <igor11.11.9988@gmail.com>
2021-12-24 16:57:20 +0200

    Added 2.js

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Header/>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+
+import Header from '../components/Header.vue'
+
+export default {
+    components: {
+        Header
+    }
+}
+</script>
+
+<style>
+
+.layout{
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-g">
+    <div class="content">
+      <div class="christmas-tree">
+        <img src="../assets/img/tree1.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
